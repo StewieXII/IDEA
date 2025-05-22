1144bf79472d9ed7ae65ce175348bf4c693f4544
Stewie XII <tu-email@ejemplo.com>
2025-05-22 20:26:17 +0200

    Aspecto visual y algunas funcionalidades

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from IDEA import app, db
+from flask import render_template
+
+@app.route('/')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    with app.app_context():  # Asegúrate de estar en el contexto de la app
-        db.create_all()  # Crear las tablas si no existen
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
